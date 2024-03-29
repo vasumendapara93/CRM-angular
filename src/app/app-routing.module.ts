28c67f7e5e4fb581c4468b17cd2b229b3e85d130
Dhyan <mrkingmoradiya@gmail,com>
2024-03-29 11:49:42 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
@@ -9,8 +10,8 @@ const routes: Routes = [
     component : LoginComponent
   },
   {
-    path: 'dashboard',
-    component : DashboardComponent
+    path: 'home',
+    component : HomeComponent
   }
 ];
 
