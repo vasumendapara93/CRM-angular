b9c556032caef9ada9582589089a06ad82490257
Dhyan <mrkingmoradiya@gmail,com>
2024-03-30 12:27:05 +0530

    side nav bar toggle functional

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,17 +7,63 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class HeaderComponent {
 
-  toggleFullScreen(){
+  toggleFullScreen() {
     var fullScreenToggleBtnIcon = document.getElementById('fullScreenToggleBtnIcon')
-    console.log("hello")
     fullScreenToggleBtnIcon?.classList.toggle('fa-compress')
     fullScreenToggleBtnIcon?.classList.toggle('fa-expand')
     if (!document.fullscreenElement) {
       if (document.documentElement.requestFullscreen) {
-          document.documentElement.requestFullscreen();
+        document.documentElement.requestFullscreen();
       }
-  } else {
-    document.exitFullscreen()
+    } else {
+      document.exitFullscreen()
+    }
   }
+
+  toggleSideNavBar() {
+    // Menu Toggle Button ( Placed in Topbar)
+    var html = document.getElementsByTagName("html")[0];
+    var view = html.getAttribute('data-sidebar-view');
+
+    if (view === 'mobile') {
+      this.showBackdrop();
+      html.classList.toggle('sidebar-open');
+    } else {
+      if (view === 'hidden') {
+        html.setAttribute("data-sidebar-view", "default");
+      } else {
+        html.setAttribute("data-sidebar-view", "hidden");
+      }
+    }
+  }
+
+  showBackdrop() {
+    const backdrop = document.createElement('div');
+    backdrop.id = 'backdrop';
+    backdrop.classList.remove(backdrop.classList.toString())
+    backdrop.classList.add('transition-all fixed inset-0 z-40 bg-gray-900 bg-opacity-50');
+    document.body.appendChild(backdrop);
+
+    if (document.getElementsByTagName('html')[0]) {
+      document.body.style.overflow = "hidden";
+      if (window.innerWidth > 1140) {
+        document.body.style.paddingRight = "17px";
+      }
+    }
+
+    const self = this
+    backdrop.addEventListener('click', function (e) {
+      document.getElementsByTagName('html')[0].classList.remove('sidebar-open');
+      self.hideBackdrop();
+    })
+  }
+
+  hideBackdrop() {
+    var backdrop = document.getElementById('backdrop');
+    if (backdrop) {
+      document.body.removeChild(backdrop);
+      document.body.style.overflow = 'null';
+      document.body.style.paddingRight = 'null';
+    }
   }
 }
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,6 +12,10 @@ class App {
         this.defaultConfig = window.config;
     }
 
+    initComponents() {
+        Waves.init()
+    }
+
     initSidenav() {
         var self = this;
         var pageUrl = window.location.href.split(/[?#]/)[0];
@@ -73,64 +77,9 @@ class App {
     }
 
 
-    initSwitchListener() {
-        var self = this;
-
-        // Menu Toggle Button ( Placed in Topbar)
-        var html = document.getElementsByTagName("html")[0];
-        var menuToggleBtn = document.querySelector('#button-toggle-menu');
-        if (menuToggleBtn) {
-            menuToggleBtn.addEventListener('click', function () {
-                var view = self.html.getAttribute('data-sidebar-view');
-
-                if (view === 'mobile') {
-                    self.showBackdrop();
-                    self.html.classList.toggle('sidebar-open');
-                } else {
-                    if (view === 'hidden') {
-                        html.setAttribute("data-sidebar-view", "default");
-                    } else {
-                        html.setAttribute("data-sidebar-view", "hidden");
-                    }
-                }
-            });
-        }
-    }
-
-
-
-    showBackdrop() {
-        const backdrop = document.createElement('div');
-        backdrop.id = 'backdrop';
-        backdrop.classList = 'transition-all fixed inset-0 z-40 bg-gray-900 bg-opacity-50';
-        document.body.appendChild(backdrop);
-
-        if (document.getElementsByTagName('html')[0]) {
-            document.body.style.overflow = "hidden";
-            if (window.innerWidth > 1140) {
-                document.body.style.paddingRight = "17px";
-            }
-        }
-
-        const self = this
-        backdrop.addEventListener('click', function (e) {
-            self.html.classList.remove('sidebar-open');
-            self.hideBackdrop();
-        })
-    }
-
-    hideBackdrop() {
-        var backdrop = document.getElementById('backdrop');
-        if (backdrop) {
-            document.body.removeChild(backdrop);
-            document.body.style.overflow = null;
-            document.body.style.paddingRight = null;
-        }
-    }
-
     init() {
+        this.initComponents()
         this.initSidenav();
-        this.initSwitchListener();
     }
 }
 
