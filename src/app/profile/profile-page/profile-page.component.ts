2c1bb01500edb033f510b66070fa783b6bc96727
DarshilSavaliya <122202933+DarshilSavaliya999@users.noreply.github.com>
2024-05-03 11:13:09 +0530

    minour changes related CSS in Profile Segment Updated (03-05-2024)

--- src/app/profile/profile-page/profile-page.component.ts
+++ src/app/profile/profile-page/profile-page.component.ts
@@ -10,16 +10,13 @@ export class ProfilePageComponent implements OnInit{
   ngOnInit(): void {
     initFlowbite();
   }
-  mobilenumber : string ="1"
-  emailAddress : string ="1"
+  mobilenumber : string =""
+  emailAddress : string =""
 
   mobileImageURLs: string[] = [
     'assets/images/profileImages/p1.svg',
     'assets/images/profileImages/p2.svg',
-    'assets/images/profileImages/p3.svg',
-    'assets/images/profileImages/p4.svg',
-    'assets/images/profileImages/p5.svg',
-    'assets/images/profileImages/p6.svg'
+    'assets/images/profileImages/p3.svg'
   ];
   mobileRandomIndex: number = Math.floor(Math.random() * this.mobileImageURLs.length);
   mobileRandomString: string = this.mobileImageURLs[this.mobileRandomIndex];
@@ -27,9 +24,7 @@ export class ProfilePageComponent implements OnInit{
   emailImageURLs: string[] = [
     'assets/images/profileImages/e1.svg',
     'assets/images/profileImages/e2.svg',
-    'assets/images/profileImages/e3.svg',
-    'assets/images/profileImages/e4.svg',
-    'assets/images/profileImages/e5.svg',
+    'assets/images/profileImages/e3.svg'
   ];
   emailRandomIndex: number = Math.floor(Math.random() * this.emailImageURLs.length);
   emailRandomString: string = this.emailImageURLs[this.emailRandomIndex];
