ee25440a0f714671028261a91379306892ab0d04
Vasu <vasumendapara93@gmail.com>
2024-06-07 10:16:39 +0530

    Create lead UI design

--- src/app/model/User.model.ts
+++ src/app/model/User.model.ts
@@ -2,26 +2,14 @@ import { Gender } from "src/assets/static/Gender"
 import IBranch from "./Branch.model"
 
 export default interface IUser{
-    id :string
+    id : number
     name : string
-    image? : string  
-    contactPerson? : string
     phoneNumber? : string
     email : string
-    abstractddress? : string,
-    roleId : string,
-    role:{
-        roleName:string
-    },
-    gender : Gender
-    isActive : boolean
-    isAccountActivated : boolean
+    password : string
+    roleId : number
+    isAccountActivate : boolean
     organizationId : string
-    branchId : string
-    branch : IBranch
-    address : string,
-    joiningDate : string
-    subscriptionStartDate : string
-    subscriptionEndDate : string,
-    createDate : string,
+    createDate : string
+    updateDate : string
 }
